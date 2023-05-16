d1d0f210aad744a85fbe77564a2199c642f8ebf0
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-05-16 15:04:49 +0100

    update

--- models/Comment.js
+++ models/Comment.js
@@ -14,6 +14,10 @@ const CommentSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Post",
     },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   });
   
   module.exports = mongoose.model("Comment", CommentSchema);
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
 
+
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
@@ -56,6 +57,7 @@ app.use(flash());
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
+
 
 //Server Running
 app.listen(process.env.PORT, () => {
