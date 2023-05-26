2c39c1194a86195349f5fcc142513ec4ae3f12bd
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-05-26 14:09:38 +0100

    debugging

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,8 +5,9 @@ const Comment = require("../models/Comment");
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const posts = await Post.find({ user: req.user.id });
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      const posts = await Post.find({ user: req.user.id })
+      
+      res.render("profile.ejs", { posts: posts, user: req.user ,});
     } catch (err) {
       console.log(err);
     }
