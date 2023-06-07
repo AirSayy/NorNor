e90c5043a7ee1365e0f450c19265279cab3b5aa6
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-06-07 17:17:26 +0100

    styling login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./.{ejs,js,html}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '0 35px 60px -15px green',
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin'),
