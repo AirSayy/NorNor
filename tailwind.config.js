add94a492ed74895c1238c3e4baa3c78851380ee
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-07-12 12:59:42 +0100

    installing tailwind cli

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./.{ejs,js,html}", "./node_modules/flowbite/**/*.js"],
+  content: ["./views/partials/.{html,ejs,js}"],
   theme: {
-    extend: {
-      boxShadow: {
-        '3xl': '0 35px 60px -15px green',
-      }
-    },
+    extend: {},
   },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
-  
+  plugins: [],
 }
 
